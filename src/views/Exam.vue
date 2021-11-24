656987451e1c620727abef556fd6938b25cbb097
huibizhang <naruto6404@hotmail.com>
2021-11-25 01:28:54 +0800

    refresh fix and don't use $router.go()

--- src/views/Exam.vue
+++ src/views/Exam.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col w-full h-screen">
-    <Header :title="title" @rightIconClicked="mode==='exam-finished'? ($router.go(1)): (examFinished())">
+    <Header :title="title" @rightIconClicked="mode==='exam-finished'? (reExam()): (examFinished())">
       <svg v-if="mode!=='exam-finished'" xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4" />
       </svg>
@@ -66,7 +66,7 @@
           <div
             v-if="mode==='exam-finished'"
             class="w-44 p-3 bg-blue-700 hover:bg-blue-500 flex gap-5 justify-center items-center text-white font-bold rounded-lg inset-0 mx-auto transition-all cursor-pointer text-lg"
-            @click="$router.go(1)"
+            @click="reExam()"
           >
             <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
@@ -116,21 +116,7 @@ export default {
       this.answerList.push('')
     }
 
-    const _me = this
-    axios({
-      method: 'post',
-      url: 'https://huibizhang.com/examler/api/getter.php',
-      data: {
-        subject: _me.subject,
-        mode: 'exam',
-        total: _me.total, 
-        count: _me.count, 
-      },
-    })
-    .then(function (response) {
-      _me.datas = response.data
-      _me.open = false
-    });
+    this.reExam()
   },
   methods: {
     init () {
@@ -231,6 +217,27 @@ export default {
     },
     recordAnswer (index, e) {
       this.answerList[index] = e
+    },
+    reExam () {
+      const _me = this
+
+      _me.mode = "exam"
+      _me.open = true
+
+      axios({
+        method: 'post',
+        url: 'https://huibizhang.com/examler/api/getter.php',
+        data: {
+          subject: _me.subject,
+          mode: 'exam',
+          total: _me.total, 
+          count: _me.count, 
+        },
+      })
+      .then(function (response) {
+        _me.datas = response.data
+        _me.open = false
+      });
     }
   },
   components: {
