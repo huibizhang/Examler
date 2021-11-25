db3bc3394c8f62f1788dbb7e71a72795ab7d4b76
huibizhang <naruto6404@hotmail.com>
2021-11-25 21:12:27 +0800

    fix total

--- vite.config.js
+++ vite.config.js
@@ -7,10 +7,13 @@ export default defineConfig({
   plugins: [
     vue(),
     VitePWA({
+      registerType: 'autoUpdate',
       includeAssets: ['favicon.png', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],  
       manifest: {
         name: '考試了 Examler',
         short_name: 'Examler',
+        // display: "fullscreen",
+        // start_url: "/index.html",
         description: '題庫練習及考試平台',
         theme_color: '#ffffff',
         icons: [
@@ -31,7 +34,7 @@ export default defineConfig({
             purpose: 'any maskable',
           }
         ]
-      }
+      },
     })
   ]
 })
