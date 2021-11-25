575afcbe5483b7304c18a162fe69e643cd896cc7
huibizhang <naruto6404@hotmail.com>
2021-11-25 21:47:03 +0800

    add registerSW

--- src/main.js
+++ src/main.js
@@ -3,12 +3,12 @@ import App from './App.vue'
 import './index.css'
 import router from "./route";
 
-// import { registerSW } from 'virtual:pwa-register'
+import { registerSW } from 'virtual:pwa-register'
 
-// const updateSW = registerSW({
-//   onNeedRefresh() {},
-//   onOfflineReady() {},
-// })
+const updateSW = registerSW({
+  onNeedRefresh() {},
+  onOfflineReady() {},
+})
 
 createApp(App)
   .use(router)
