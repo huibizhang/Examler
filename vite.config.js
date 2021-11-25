659d2cfe0f57228fb2bb947e14b62409d36343d4
huibizhang <naruto6404@hotmail.com>
2021-11-25 19:50:39 +0800

    recovery to older version

--- vite.config.js
+++ vite.config.js
@@ -9,68 +9,29 @@ export default defineConfig({
     VitePWA({
       includeAssets: ['favicon.png', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],  
       manifest: {
-        // name: '考試了 Examler',
-        // short_name: 'Examler',
-        // start_url: "/index.html",
-        // description: '題庫練習及考試平台',
-        // theme_color: '#ffffff',
-        // icons: [
-        //   {
-        //     src: 'pwa-192x192.png',
-        //     sizes: '192x192',
-        //     type: 'image/png',
-        //   },
-        //   {
-        //     src: 'pwa-512x512.png',
-        //     sizes: '512x512',
-        //     type: 'image/png',
-        //   },
-        //   {
-        //     src: 'pwa-512x512.png',
-        //     sizes: '512x512',
-        //     type: 'image/png',
-        //     purpose: 'any maskable',
-        //   }
-        // ]
-
-        "background_color": "#ffffff",
-        "description": "題庫練習及考試平台",
-        "dir": "ltr",
-        "display": "standalone",
-        "name": "考試了 Examler",
-        "orientation": "any",
-        "scope": "/",
-        "short_name": "Examler",
-        "start_url": "/index.html",
-        "theme_color": "#ffffff",
-        "categories": [],
-        "screenshots": [],
-        "icons": [
+        name: '考試了 Examler',
+        short_name: 'Examler',
+        start_url: "/index.html",
+        description: '題庫練習及考試平台',
+        theme_color: '#ffffff',
+        icons: [
           {
-            "src": "/favicon-32x32.png",
-            "type": "image/png",
-            "sizes": "32x32"
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
           },
           {
-            "src": "/favicon-16x16.png",
-            "type": "image/png",
-            "sizes": "16x16"
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
           },
           {
-            "src": "/apple-touch-icon.png",
-            "type": "image/png",
-            "sizes": "180x180"
-          },
-          {
-            "src": "/safari-pinned-tab.svg"
-          },
-          {
-            "src": "/favicon.ico",
-            "type": "image/x-icon"
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
           }
-        ],
-        "shortcuts": [] 
-      },
+        ]
     })
   ]
 })
